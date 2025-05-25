6d5f140b3d2d2f779fdfbd04f7d700640b17a9a4
Harikishan-AI <harikishantakoor@gmail.com>
2025-05-25 13:45:53 +0530

    working on common functionalities

--- src/mlops_project/utils/common.py
+++ src/mlops_project/utils/common.py
@@ -32,3 +32,71 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
     except Exception as e:
         logger.error(f"Error reading YAML file at {path_to_yaml}: {e}")
         raise e
+
+@ensure_annotations
+def create_directories(paths: list):
+    """
+    Creates directories if they do not exist.
+    
+    Args:
+        paths (list): List of directory paths to create.
+    """
+    for path in paths:
+        os.makedirs(path, exist_ok=True)
+        logger.info(f"Directory created: {path}")
+
+@ensure_annotations
+def save_json(path: Path, data: Any):
+    """
+    Saves data to a JSON file.
+    
+    Args:
+        path (Path): Path to the JSON file.
+        data (Any): Data to be saved in JSON format.
+    """
+    with open(path, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+        logger.info(f"Data saved to JSON file at {path}")
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """
+    Loads data from a JSON file.
+    
+    Args:
+        path (Path): Path to the JSON file.
+        
+    Returns:
+        Any: Data loaded from the JSON file.
+    """
+    with open (path, 'r') as json_file:
+        data = json.load(json_file)
+        logger.info(f"Data loaded from JSON file at {path}")
+        return ConfigBox(data)
+
+@ensure_annotations
+def save_bin(path: Path, data: Any):
+    """
+    Saves data to a binary file using joblib.
+    
+    Args:
+        path (Path): Path to the binary file.
+        data (Any): Data to be saved in binary format.
+    """
+    joblib.dump(data, path)
+    logger.info(f"Data saved to binary file at {path}")
+
+@ensure_annotations
+def job_lib_load(path: Path) -> Any:
+    """
+    Loads data from a binary file using joblib.
+    
+    Args:
+        path (Path): Path to the binary file.
+        
+    Returns:
+        Any: Data loaded from the binary file.
+    """
+    data = joblib.load(path)
+    logger.info(f"Data loaded from binary file at {path}")
+    return data
